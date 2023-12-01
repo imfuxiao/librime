ac62f90dd422b3733b752508113f2f4ab215892f
fu.xiao <im.fu.xiao@gmail.com>
2023-12-01 17:36:22 +0800

    feat: support iOS platform build

--- deps.mk
+++ deps.mk
@@ -39,6 +39,7 @@ glog:
 	-DWITH_GFLAGS:BOOL=OFF \
 	-DCMAKE_BUILD_TYPE:STRING="Release" \
 	-DCMAKE_INSTALL_PREFIX:PATH="$(prefix)" \
+	$(RIME_CMAKE_FLAGS) \
 	&& cmake --build $(build) --target install
 
 googletest:
@@ -47,6 +48,7 @@ googletest:
 	-DBUILD_GMOCK:BOOL=OFF \
 	-DCMAKE_BUILD_TYPE:STRING="Release" \
 	-DCMAKE_INSTALL_PREFIX:PATH="$(prefix)" \
+	$(RIME_CMAKE_FLAGS) \
 	&& cmake --build $(build) --target install
 
 leveldb:
@@ -56,6 +58,7 @@ leveldb:
 	-DLEVELDB_BUILD_TESTS:BOOL=OFF \
 	-DCMAKE_BUILD_TYPE:STRING="Release" \
 	-DCMAKE_INSTALL_PREFIX:PATH="$(prefix)" \
+	$(RIME_CMAKE_FLAGS) \
 	&& cmake --build $(build) --target install
 
 marisa-trie:
@@ -66,15 +69,40 @@ marisa-trie:
 	-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON \
 	-DBUILD_TESTING:BOOL=OFF \
 	-DENABLE_TOOLS:BOOL=OFF \
+	$(RIME_CMAKE_FLAGS) \
 	&& cmake --build $(build) --target install
 
 opencc:
+ifndef RIME_IOS_CROSS_COMPILING
 	cd $(src_dir)/opencc; \
 	cmake . -B$(build) \
 	-DBUILD_SHARED_LIBS:BOOL=OFF \
 	-DCMAKE_BUILD_TYPE:STRING="Release" \
 	-DCMAKE_INSTALL_PREFIX:PATH="$(prefix)" \
+	$(RIME_CMAKE_FLAGS) \
 	&& cmake --build $(build) --target install
+else
+	# 对于iOS交叉编译，opencc是一个特例。
+	# opencc产生libopencc.a 和 可执行opencc_dict，构建时用来它生成字典。
+	# 这意味着我们必须编译两次opencc。
+	# 首先针对主机可执行文件 opencc-dict 生成字典。
+	# 然后交叉编译生成libopencc.a
+	cd $(src_dir)/opencc; \
+	cmake . -B$(build) \
+	-DBUILD_SHARED_LIBS:BOOL=OFF \
+	-DCMAKE_BUILD_TYPE:STRING="Release" \
+	-DCMAKE_INSTALL_PREFIX:PATH="$(prefix)" \
+	&& cmake --build build --target install \
+	&& rm $(prefix)/lib/libopencc.a || true \
+	&& echo "Cross compiling..." \
+	&& export PATH=$(prefix)/bin:$$PATH && echo $$PATH \
+	&& cmake . -B$(build)/opencc_arm64 \
+	-DBUILD_SHARED_LIBS:BOOL=OFF \
+	-DCMAKE_BUILD_TYPE:STRING="Release" \
+	-DCMAKE_INSTALL_PREFIX:PATH="$(prefix)" \
+	$(RIME_CMAKE_FLAGS) \
+	&& cmake --build $(build)/opencc_arm64 --target install
+endif
 
 yaml-cpp:
 	cd $(src_dir)/yaml-cpp; \
@@ -84,4 +112,5 @@ yaml-cpp:
 	-DYAML_CPP_BUILD_TOOLS:BOOL=OFF \
 	-DCMAKE_BUILD_TYPE:STRING="Release" \
 	-DCMAKE_INSTALL_PREFIX:PATH="$(prefix)" \
+	$(RIME_CMAKE_FLAGS) \
 	&& cmake --build $(build) --target install
